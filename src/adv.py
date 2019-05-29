e780941ebbc003a9e6392031afbfc751b1b1acef
Jordan Stoddard <jjsincorporated@gmail.com>
2019-05-29 14:49:11 -0700

    Add items to each room instance

--- src/adv.py
+++ src/adv.py
@@ -6,21 +6,21 @@ from player import Player
 # This is a dictionary where the key is a the name of a room, and the value is the invocation of a Room class, with two strings passed in a parameters.
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", ""),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", ""),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", ""),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", ""),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", ""),
 }
 
 
@@ -42,7 +42,7 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-player = Player(room['outside'])
+player = Player(room['outside'], 'Bart')
 
 
 """
@@ -67,6 +67,8 @@ while True:
     print(player.current_room.name)
 # * Prints the current description (the textwrap module might be useful here).
     print(player.current_room.description)
+    print(f"Items in this room: {player.current_room.item_list}")
+
 # * Waits for user input and decides what to do
     string = input('\nEnter a command: ').lower().split()
     if len(string) == 1:
