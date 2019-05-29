e7eb0458d4aa21af7cacf2cadfc9aae208d82d95
Jordan Stoddard <jjsincorporated@gmail.com>
2019-05-29 16:32:45 -0700

    add inventory_list to player

--- src/player.py
+++ src/player.py
@@ -2,9 +2,10 @@
 # currently.
 
 class Player:
-    def __init__(self, current_room, name):
+    def __init__(self, current_room, name, inventory_list):
         self.current_room = current_room
         self.name = name
+        self.inventory_list = inventory_list
 
     def __str__(self):
-        return f"Player name: {self.name}, Current room: {self.current_room}"
+        return f"Player name: {self.name}, Current room: {self.current_room}, Inventory: {self.inventory_list}"
