f02a4d298414e8bbbad14bf38f1b46e9a9a0e232
Jordan Stoddard <jjsincorporated@gmail.com>
2019-05-29 14:50:05 -0700

    Add item_list to Room class

--- src/room.py
+++ src/room.py
@@ -2,9 +2,10 @@
 # description attributes.
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, item_list):
         self.name = name
         self.description = description
+        self.item_list = item_list
 
     def __str__(self):
-        return f"{self.name} {self.description}"
+        return f"Room name:{self.name},\nRoom description:{self.description}\nItems in this room: {self.item_list}"
