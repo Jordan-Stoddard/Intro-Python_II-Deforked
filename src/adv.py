36c68bb711f60ae9236c00d186b55b42c5acd29e
Jordan Stoddard <jjsincorporated@gmail.com>
2019-05-30 13:24:44 -0700

    MVP day 2 complete.

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,6 @@
 from room import Room
 from player import Player
+from item import Item
 from helper_functions import handle_direction
 
 # Declare all the rooms
@@ -7,10 +8,10 @@ from helper_functions import handle_direction
 # This is a dictionary where the key is a the name of a room, and the value is the invocation of a Room class, with two strings passed in a parameters.
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons", ["pebble", "stick", "dougnut"]),
+                     "North of you, the cave mount beckons", [Item("pebble"), Item("stick"), Item("doughnut")]),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east.""", ""),
+passages run north and east.""", []),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
@@ -45,26 +46,34 @@ room['treasure'].s_to = room['narrow']
 # Make a new player object that is currently in the 'outside' room.
 player = Player(room['outside'], 'Bart', [])
 
-def handle_action(input, current_room, player):
+def get_item(input, player):
     if input[0] == 'get' or input[0] == 'take':
-        match = [i for i in current_room.item_list if input[1] in current_room.item_list]
+        item_list = [i.name for i in player.current_room.item_list]
+        match = [i  for i in item_list  if i == input[1]]
         try:
-            return match[0]
+            return ['getItem', match[0]]
         except IndexError:
-            print(f"\n**Sorry, but that item doesn't exist in this room. Try something else.**\n")
+            print(f"\n**Sorry, but that item doesn't exist in this room. Try something else.**")
+
+    elif input[0] == 'drop':
+        inventory_list = [i.name for i in player.inventory_list]
+        match = [i  for i in inventory_list  if i == input[1]]
+        try:
+            return ['dropItem', match[0]]
+        except IndexError:
+            print(f"\n**Sorry, but that item doesn't exist in your inventory. Try something else.**")
+
     else:
         print("""
         ** Invalid two word command.**
-        Two word command list: get [item], take [item]\n""")
+        Two word command list: get [item], take [item], drop [item]\n""")
 
 
 # Write a loop that:
 #
 while True:
 # * Prints the current room name, description, and items in the room.
-    print(player.current_room.name)
-    print(player.current_room.description)
-    print(f"Items in this area: {', '.join(player.current_room.item_list)}")
+    print(player.current_room)
 
 # * Waits for user input and decides what to do
     string = input('\nEnter a command: ').lower().split()
@@ -75,12 +84,34 @@ while True:
         if string[0] == 'q':
             print("\nWe'll see you next time, adventurer.")
             break
+# If hte user enters "q" or "inventory" show player's current inventory.
+        elif string[0] == 'i' or string[0] == 'inventory':
+            inventory = [i.name for i in player.inventory_list]
+            print(f"\n Your current inventory: {', '.join(inventory)}\n")
 # If user enters valid direction: n, s, e, w then run handle_direction
-        player.current_room = handle_direction(string[0], player.current_room)
+        else:
+            player.current_room = handle_direction(string[0], player.current_room)
         
 # Check if command is 2 words.
     elif len(string) == 2:
-        handle_action(string, player.current_room)
+        item = get_item(string, player)
+        try:
+            if item[0] == 'getItem':
+                player.current_room.item_list = [i.name if i.name == item[1] else i  for i in player.current_room.item_list]
+                player.current_room.item_list = [True if i == item[1] else i  for i in player.current_room.item_list]              
+                player.current_room.item_list.remove(True)
+                player.inventory_list.append(Item(item[1]))
+                print(f"\n**You picked up the {item[1]}!**\n It's now in your inventory.\n Try i or inventory to see your current inventory.\n")
+            elif item[0] == 'dropItem':
+                print('Went into getDrop')
+                player.inventory_list = [i.name if i.name == item[1] else i  for i in player.inventory_list]
+                player.inventory_list = [True if i == item[1] else i  for i in player.inventory_list]              
+                player.inventory_list.remove(True)
+                player.current_room.item_list.append(Item(item[1]))
+                print(player.inventory_list)
+                print(f"\n**You dropped your {item[1]}!**\n")
+        except TypeError:
+            print('')
     else:
         print('\n**Invalid number of commands given.** \n Please either give a direction(n, s, e, w), or a two word command.\n')
 
